7439f8415568382a951c8d77282e9ed0cf6f770b
sindhur-cpu <sindhuja.r.patlola@gmail.com>
2026-01-16 19:58:18 +0530

    Add files via upload

---
+++ Index.tsx
@@ -0,0 +1,17 @@
+import Header from '@/components/Header';
+import ConversionPanel from '@/components/ConversionPanel';
+import Footer from '@/components/Footer';
+
+const Index = () => {
+  return (
+    <div className="min-h-screen flex flex-col">
+      <Header />
+      <main className="flex-1">
+        <ConversionPanel />
+      </main>
+      <Footer />
+    </div>
+  );
+};
+
+export default Index;
